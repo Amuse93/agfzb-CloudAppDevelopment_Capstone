98a230c7d4ac13adb91f79662e3725568f90b776
Amuse93 <andrewnmuse@yahoo.com>
2022-07-15 12:57:52 +0000

    Added CarMake and CarModel to admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInLine(admin.StackedInline):
+    model = CarModel
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'carmake', 'year', 'type', 'dealer_id')
+    list_filter = ['name']
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInLine]
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -10,6 +10,14 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+class CarMake (models.Model):
+  name = models.CharField(null=False, max_length=30, default='make')
+  description = models.CharField(max_length=1000)
+  
+  def __str__(self):
+    return "Name: " + self.name + "," + \
+           "Description: " + self.description
+
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,6 +27,42 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+  SEDAN = 'sedan'
+  COUPE = 'coupe'
+  SPORTSCAR = 'sports car'
+  STATIONWAGON = 'station wagon'
+  HATCHBACK = 'hatchback'
+  CONVERTABLE = 'convertable'
+  SUV = 'SUV'
+  MINIVAN = 'minivan'
+  PICKUPTRUCK = 'pickup truck'
+  MODEL_TYPES = [
+      (SEDAN, 'Sedan'),
+      (COUPE, 'Coupe'),
+      (SPORTSCAR, 'Sports Car'),
+      (STATIONWAGON, 'Station Wagon'),
+      (HATCHBACK, 'Hatchback'),
+      (CONVERTABLE, 'Convertable'),
+      (SUV,'SUV'),
+      (MINIVAN,'Minivan'),
+      (PICKUPTRUCK, 'Pickup Truck')
+  ]
+  
+  
+  carmake = models.ForeignKey(CarMake,on_delete=models.CASCADE)
+  dealer_id = models.IntegerField(default=0)
+  name = models.CharField(null=False, max_length=30, default='model')
+  type = models.CharField(max_length=15, choices=MODEL_TYPES, default=SEDAN)
+  year = models.DateField(default=now)
+  
+def __str__(self):
+    return "Name: " + self.name + "," + \
+           "Car Make: " + self.carmake + "," + \
+           "Dealer_id: " + self.dealer_id + "," \
+           "Type: " + self.type + "," + \
+           "Year: " + self.year + "."
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
